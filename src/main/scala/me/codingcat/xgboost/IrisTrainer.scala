69d880bf21ab16090f0c3c401f71997a0c4a2129
Nan Zhu <nanzhu@uber.com>
2018-07-25 15:26:03 -0700

    basic version

--- src/main/scala/me/codingcat/xgboost/IrisTrainer.scala
+++ src/main/scala/me/codingcat/xgboost/IrisTrainer.scala
@@ -1,8 +1,49 @@
 package me.codingcat.xgboost
 
+import ml.dmlc.xgboost4j.scala.spark.XGBoostClassifier
+
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
+
 object IrisTrainer {
 
+
+
   def main(args: Array[String]): Unit = {
-    
+    val spark = SparkSession.builder().getOrCreate()
+    val schema = new StructType(Array(
+      StructField("sepal length", DoubleType, true),
+      StructField("sepal width", DoubleType, true),
+      StructField("petal length", DoubleType, true),
+      StructField("petal width", DoubleType, true),
+      StructField("class", StringType, true)))
+    val rawInput = spark.read.schema(schema).csv(args(0))
+
+    // transform class to index to make xgboost happy
+    val stringIndexer = new StringIndexer()
+      .setInputCol("class")
+      .setOutputCol("classIndex")
+      .fit(rawInput)
+    val labelTransformed = stringIndexer.transform(rawInput).drop("class")
+    // compose all feature columns as vector
+    val vectorAssembler = new VectorAssembler().
+      setInputCols(Array("sepal length", "sepal width", "petal length", "petal width")).
+      setOutputCol("features")
+    val xgbInput = vectorAssembler.transform(labelTransformed).select("features",
+      "classIndex")
+
+    val xgbParam = Map("eta" -> 0.1f,
+      "max_depth" -> 2,
+      "objective" -> "multi:softprob",
+      "num_class" -> 3,
+      "num_round" -> 100,
+      "nWorkers" -> 2)
+    val xgbClassifier = new XGBoostClassifier(xgbParam).
+      setFeaturesCol("features").
+      setLabelCol("classIndex")
+    val xgbClassificationModel = xgbClassifier.fit(xgbInput)
+    val results = xgbClassificationModel.transform(xgbInput)
+    results.show()
   }
 }
